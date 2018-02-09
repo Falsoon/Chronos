a0c9a300094b98bef6dc3cb72d8740ebe22ae1de
Daniel <Daniel@172.31.49.160>
2018-02-09 14:20:36 -0500

    AUTHOR DRAWING ROOMS: Grid added and drawing of rooms

--- ArenaMapMakerAuthor/src/MapPanel.java
+++ ArenaMapMakerAuthor/src/MapPanel.java
@@ -1,31 +1,50 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.event.*;
+import java.awt.geom.GeneralPath;
+
 import javax.swing.JPanel;
 
-public class MapPanel extends JPanel implements MouseListener{
-	
-	private boolean paintRooms;
+public class MapPanel extends JPanel implements MouseListener {
+
+	private boolean drawing, creating;
+	private MouseHandler mousehandler = new MouseHandler();
+	public GeneralPath path;
 
 	public MapPanel() {
-		this.setBackground(Color.black);
-		addMouseListener(this);
+		addMouseListener(mousehandler);
+		addMouseMotionListener(mousehandler);
 	}
 
 	public void paintRooms() {
-		paintRooms = true;
+		creating = true;
 	}
-	
+
 	public void clear() {
-		paintRooms = false;
+		creating = false;
 	}
 
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.black);
+		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(0, 0, 600, 600);
-		if(paintRooms) {
-			g.setColor(Color.white);
-			g.drawString("....................", 100, 100);
+		g.setColor(Color.white);
+		for (int i = 0; i < 600; i++) {
+			for (int j = 0; j < 600; j++) {
+				g.drawLine(10 * (i + 1), 10 * (j + 1), 10 * (i + 1), 10 * (j + 1));
+			}
+		}
+		if (creating) {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setColor(Color.BLACK);
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			g2d.setStroke(new BasicStroke(8, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL));
+			if (path != null) {
+				g2d.draw(path);
+			}
 		}
 	}
 
@@ -37,25 +56,41 @@ public class MapPanel extends JPanel implements MouseListener{
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
+	private class MouseHandler extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			if(creating) {
+			Point p = e.getPoint();
+			if (!drawing) {
+				path = new GeneralPath();
+				path.moveTo(p.x, p.y);
+				drawing = true;
+			} else {
+				path.lineTo(p.x, p.y);
+			}
+			repaint();
+			}
+		}
+	}
 }
