838bfdd0ad651e356459b54541ea9e8c4cb1d897
tdelgado37 <tdelgado37.td@gmail.com>
2018-03-07 13:20:41 -0500

    Add a test case to description form test file

--- ArenaMapMakerAuthor/Tests/DescriptionFormTesting.java
+++ ArenaMapMakerAuthor/Tests/DescriptionFormTesting.java
@@ -1,8 +1,16 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import civ.CIV;
 import civ.FormCiv;
 import hic.FormWindow;
 import pdc.Room;
@@ -12,21 +20,62 @@ class DescriptionFormTesting {
 
 	
 	private Room room = new Room();
-	private FormCiv fc = new FormCiv();
+	private FormCiv fc = new FormCiv() ;
 	private FormWindow fw = new FormWindow(fc, false);
+	private JTextArea textArea, titleText;
+	private JFrame frame;
+
+	@AfterEach
+	public void tearDown() {
+		room = null;
+		fc = null;
+		fw = null;
+		RoomList.reset();
+	}
 	
 	@Test
 	void testRoomID() {
 		RoomList.add(room);
+		int roomID = room.ROOMID;
 		fc.setRoomReference(room.toString());
-		int ans = room.ROOMID;
-		
-		assertEquals(ans, fc.getRoomID() );		
+		assertEquals(roomID, fc.getRoomID() );		
 	}
 	
 	@Test
-	void testRoomTitle(){
-		room.title = "hello";
+	void testNumberInputForDescAndTitle(){
+		
+		RoomList.add(room);
+		fc.setRoomReference(room.toString());
+		
+		frame = new JFrame();
+		frame.setBounds(200, 100, 350, 600);
+		frame.setTitle("Room Description");
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblRoomTitle = new JLabel("Room Title");
+		lblRoomTitle.setBounds(10, 25, 100, 15);
+		frame.getContentPane().add(lblRoomTitle);
+		
+		
+		
+		titleText = new JTextArea();
+		titleText.setBounds(110, 20, 200, 30);
+		frame.getContentPane().add(titleText);
+		titleText.setColumns(10);
+		titleText.setText("1234");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 100, 300, 400);
+		frame.getContentPane().add(scrollPane);
+		
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setText("1234");
+		
+		fc.adjustRoomTitleAndDesc(titleText.getText(), textArea.getText() );
+		
+		assertEquals("1234", room.title );
+		assertEquals("1234", room.desc );
 		
 	}
 
