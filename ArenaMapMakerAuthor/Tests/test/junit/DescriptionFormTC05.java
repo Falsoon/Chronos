fbd695fba48a4909871159102f9197a69daed989
tdelgado37 <tdelgado37.td@gmail.com>
2018-03-19 11:11:51 -0400

    additions to TC05 testing file

--- ArenaMapMakerAuthor/Tests/test/junit/DescriptionFormTC05.java
+++ ArenaMapMakerAuthor/Tests/test/junit/DescriptionFormTC05.java
@@ -2,6 +2,9 @@ package test.junit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -13,6 +16,8 @@ import org.junit.jupiter.api.Test;
 
 import civ.CIV;
 import civ.FormCiv;
+import hic.AuthorWindow;
+import hic.ButtonFactory;
 import hic.FormWindow;
 import pdc.Room;
 import pdc.RoomList;
@@ -23,6 +28,8 @@ class DescriptionFormTC05 {
 	private Room room = new Room();
 	private FormCiv fc = new FormCiv() ;
 	private FormWindow fw = new FormWindow(fc, false);
+	private AuthorWindow aw = new AuthorWindow();
+	private ButtonFactory bf = new ButtonFactory(aw);
 	private JTextArea textArea, titleText;
 	private JFrame frame;
 
@@ -31,28 +38,73 @@ class DescriptionFormTC05 {
 		room = null;
 		fc = null;
 		fw = null;
+		aw = null;
+		bf = null;
 		RoomList.reset();
-		System.out.print("yo i done");
 	}
 	
 	@Test
 	void testAuthorSelectsDescriptionMode() {
-		
+		bf = new ButtonFactory(aw);
+		bf.setMode1();
+		assertEquals(false, aw.wallCombo.isVisible());
+		assertEquals(false, aw.portalCombo.isVisible());
 	}
 	
 	@Test
 	void testAuthorSelectsDrawingMode() {
-		
+		bf = new ButtonFactory(aw);
+		bf.setMode2();
+		assertEquals(true, aw.wallCombo.isVisible());
+		assertEquals(true, aw.portalCombo.isVisible());
 	}
 	
 	@Test
 	void testCreateRoomViaForm() {
+		bf = new ButtonFactory(aw);
+		bf.setMode1();
+		bf.addRoombtn1.doClick();
+		assertEquals(true, fw.frame.isVisible());
 		
 	}
 	
 	@Test
 	void testClosesFormBeforeSubmit() {
 		//the room title and room description should not save
+		RoomList.add(room);
+		fc.setRoomReference(room.toString());
+		
+		frame = new JFrame();
+		frame.setBounds(200, 100, 350, 600);
+		frame.setTitle("Room Description");
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblRoomTitle = new JLabel("Room Title");
+		lblRoomTitle.setBounds(10, 25, 100, 15);
+		frame.getContentPane().add(lblRoomTitle);
+		
+		
+		
+		titleText = new JTextArea();
+		titleText.setBounds(110, 20, 200, 30);
+		frame.getContentPane().add(titleText);
+		titleText.setColumns(10);
+		titleText.setText("1234");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 100, 300, 400);
+		frame.getContentPane().add(scrollPane);
+		
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setText("1234");
+		
+		frame.setVisible(false);
+		frame.dispose();
+		
+		assertEquals("", room.title );
+		assertEquals("", room.desc );
+		
 	}
 	
 	@Test
